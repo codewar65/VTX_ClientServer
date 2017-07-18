fc48f120a178ae2a513168dee0101657c4a3b228
Daniel Mecklenburg Jr <mecklenburgd@gmail.com>
2017-07-17 21:34:11 -0400

    Font corrections
    Defaulting to built in fonts.

--- www/vtxclient.js
+++ www/vtxclient.js
@@ -510,6 +510,193 @@ var
     shiftState, ctrlState, altState,
     numState, capState, scrState;
 
+/**
+ * JavaScript Client Detection
+ * (C) viazenetti GmbH (Christian Ludwig)
+ */
+(function (window) {
+    {
+        var unknown = '-';
+
+        // screen
+        var screenSize = '';
+        if (screen.width) {
+            width = (screen.width) ? screen.width : '';
+            height = (screen.height) ? screen.height : '';
+            screenSize += '' + width + " x " + height;
+        }
+
+        // browser
+        var nVer = navigator.appVersion;
+        var nAgt = navigator.userAgent;
+        var browser = navigator.appName;
+        var version = '' + parseFloat(navigator.appVersion);
+        var majorVersion = parseInt(navigator.appVersion, 10);
+        var nameOffset, verOffset, ix;
+
+        // Opera
+        if ((verOffset = nAgt.indexOf('Opera')) != -1) {
+            browser = 'Opera';
+            version = nAgt.substring(verOffset + 6);
+            if ((verOffset = nAgt.indexOf('Version')) != -1) {
+                version = nAgt.substring(verOffset + 8);
+            }
+        }
+        // Opera Next
+        if ((verOffset = nAgt.indexOf('OPR')) != -1) {
+            browser = 'Opera';
+            version = nAgt.substring(verOffset + 4);
+        }
+        // Edge
+        else if ((verOffset = nAgt.indexOf('Edge')) != -1) {
+            browser = 'Microsoft Edge';
+            version = nAgt.substring(verOffset + 5);
+        }
+        // MSIE
+        else if ((verOffset = nAgt.indexOf('MSIE')) != -1) {
+            browser = 'Microsoft Internet Explorer';
+            version = nAgt.substring(verOffset + 5);
+        }
+        // Chrome
+        else if ((verOffset = nAgt.indexOf('Chrome')) != -1) {
+            browser = 'Chrome';
+            version = nAgt.substring(verOffset + 7);
+        }
+        // Safari
+        else if ((verOffset = nAgt.indexOf('Safari')) != -1) {
+            browser = 'Safari';
+            version = nAgt.substring(verOffset + 7);
+            if ((verOffset = nAgt.indexOf('Version')) != -1) {
+                version = nAgt.substring(verOffset + 8);
+            }
+        }
+        // Firefox
+        else if ((verOffset = nAgt.indexOf('Firefox')) != -1) {
+            browser = 'Firefox';
+            version = nAgt.substring(verOffset + 8);
+        }
+        // MSIE 11+
+        else if (nAgt.indexOf('Trident/') != -1) {
+            browser = 'Microsoft Internet Explorer';
+            version = nAgt.substring(nAgt.indexOf('rv:') + 3);
+        }
+        // Other browsers
+        else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) < (verOffset = nAgt.lastIndexOf('/'))) {
+            browser = nAgt.substring(nameOffset, verOffset);
+            version = nAgt.substring(verOffset + 1);
+            if (browser.toLowerCase() == browser.toUpperCase()) {
+                browser = navigator.appName;
+            }
+        }
+        // trim the version string
+        if ((ix = version.indexOf(';')) != -1) version = version.substring(0, ix);
+        if ((ix = version.indexOf(' ')) != -1) version = version.substring(0, ix);
+        if ((ix = version.indexOf(')')) != -1) version = version.substring(0, ix);
+
+        majorVersion = parseInt('' + version, 10);
+        if (isNaN(majorVersion)) {
+            version = '' + parseFloat(navigator.appVersion);
+            majorVersion = parseInt(navigator.appVersion, 10);
+        }
+
+        // mobile version
+        var mobile = /Mobile|mini|Fennec|Android|iP(ad|od|hone)/.test(nVer);
+
+        // cookie
+        var cookieEnabled = (navigator.cookieEnabled) ? true : false;
+
+        if (typeof navigator.cookieEnabled == 'undefined' && !cookieEnabled) {
+            document.cookie = 'testcookie';
+            cookieEnabled = (document.cookie.indexOf('testcookie') != -1) ? true : false;
+        }
+
+        // system
+        var os = unknown;
+        var clientStrings = [
+            {s:'Windows 10', r:/(Windows 10.0|Windows NT 10.0)/},
+            {s:'Windows 8.1', r:/(Windows 8.1|Windows NT 6.3)/},
+            {s:'Windows 8', r:/(Windows 8|Windows NT 6.2)/},
+            {s:'Windows 7', r:/(Windows 7|Windows NT 6.1)/},
+            {s:'Windows Vista', r:/Windows NT 6.0/},
+            {s:'Windows Server 2003', r:/Windows NT 5.2/},
+            {s:'Windows XP', r:/(Windows NT 5.1|Windows XP)/},
+            {s:'Windows 2000', r:/(Windows NT 5.0|Windows 2000)/},
+            {s:'Windows ME', r:/(Win 9x 4.90|Windows ME)/},
+            {s:'Windows 98', r:/(Windows 98|Win98)/},
+            {s:'Windows 95', r:/(Windows 95|Win95|Windows_95)/},
+            {s:'Windows NT 4.0', r:/(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/},
+            {s:'Windows CE', r:/Windows CE/},
+            {s:'Windows 3.11', r:/Win16/},
+            {s:'Android', r:/Android/},
+            {s:'Open BSD', r:/OpenBSD/},
+            {s:'Sun OS', r:/SunOS/},
+            {s:'Linux', r:/(Linux|X11)/},
+            {s:'iOS', r:/(iPhone|iPad|iPod)/},
+            {s:'Mac OS X', r:/Mac OS X/},
+            {s:'Mac OS', r:/(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/},
+            {s:'QNX', r:/QNX/},
+            {s:'UNIX', r:/UNIX/},
+            {s:'BeOS', r:/BeOS/},
+            {s:'OS/2', r:/OS\/2/},
+            {s:'Search Bot', r:/(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/}
+        ];
+        for (var id in clientStrings) {
+            var cs = clientStrings[id];
+            if (cs.r.test(nAgt)) {
+                os = cs.s;
+                break;
+            }
+        }
+
+        var osVersion = unknown;
+
+        if (/Windows/.test(os)) {
+            osVersion = /Windows (.*)/.exec(os)[1];
+            os = 'Windows';
+        }
+
+        switch (os) {
+            case 'Mac OS X':
+                osVersion = /Mac OS X (10[\.\_\d]+)/.exec(nAgt)[1];
+                break;
+
+            case 'Android':
+                osVersion = /Android ([\.\_\d]+)/.exec(nAgt)[1];
+                break;
+
+            case 'iOS':
+                osVersion = /OS (\d+)_(\d+)_?(\d+)?/.exec(nVer);
+                osVersion = osVersion[1] + '.' + osVersion[2] + '.' + (osVersion[3] | 0);
+                break;
+        }
+
+        // flash (you'll need to include swfobject)
+        /* script src="//ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js" */
+        var flashVersion = 'no check';
+        if (typeof swfobject != 'undefined') {
+            var fv = swfobject.getFlashPlayerVersion();
+            if (fv.major > 0) {
+                flashVersion = fv.major + '.' + fv.minor + ' r' + fv.release;
+            }
+            else  {
+                flashVersion = unknown;
+            }
+        }
+    }
+
+    window.jscd = {
+        screen: screenSize,
+        browser: browser,
+        browserVersion: version,
+        browserMajorVersion: majorVersion,
+        mobile: mobile,
+        os: os,
+        osVersion: osVersion,
+        cookies: cookieEnabled,
+        flashVersion: flashVersion
+    };
+}(this));
+    
 function isMicrosoft() {
     var 
         uA = window.navigator.userAgent,
@@ -521,7 +708,7 @@ function isMicrosoft() {
     return !isNaN(checkVersion);
 }
 
-if (isMicrosoft()) {
+if (jscd.browser.split(' ')[0] == 'Microsoft') {
     document.write('VTX is not compatible with Microsoft browsers.<br>');
     document.write('Please use a Mozilla or Chromium browser.');
     exit;
@@ -762,8 +949,7 @@ function conPutChar(rownum, colnum, chr, attr) {
     expandToCol(rownum, colnum);
     conText[rownum] = conText[rownum].splice(colnum, 1, String.fromCharCode(chr));
     conCellAttr[rownum][colnum] = attr;
-
-renderCell(rownum, colnum);
+    renderCell(rownum, colnum);
 }
 
 // output character using current attribute at cursor position.
@@ -1291,7 +1477,7 @@ function conCharOut(chr) {
                 var c2 = clut[parm[1]];
                 switch (parm[2] << 16) {
                     case A_ROW_SOLID:
-                        row.style['background-color'] = c1;
+                        row.style['background'] = c1;
                         break;
 
                     case A_ROW_HORZ:
@@ -1995,11 +2181,11 @@ function adjustRow(rownum) {
         row.appendChild(cnv);
     }
     
-    if (cnv.height != h + 8) {
+    if (cnv.height != (h + 16)) {
         // adjust for new height.
         row.style['height'] = h + 'px';
         cnv.width = 80 * colSize;
-        cnv.height = h + 8;
+        cnv.height = (h + 16);
         // redraw this entire row
         for (i = 0; i < conText[rownum].length; i++)
             renderCell(rownum, i);
@@ -2009,14 +2195,16 @@ function adjustRow(rownum) {
 // render an individual row, col
 function renderCell(rownum, colnum) {
     var
-        row, cnv;
+    row, size, width, w, h, x, y,
+    ctx, attr, ch, tfg, tbg, tbold, stroke, tmp;
         
     row = getRowElement(rownum);
     size = getRowAttrSize(conRowAttr[rownum]) / 100;    // .25 - 2
     width = getRowAttrWidth(conRowAttr[rownum])/ 100;   // .5 - 2
-    h = rowSize * size;             // height of char
     w = colSize * size * width;     // width of char
+    h = rowSize * size;             // height of char
     x = w * colnum;                 // left pos of char on canv
+    y = 0;
 
     cnv = row.firstChild;           // get canvas
     if (!cnv) {
@@ -2025,19 +2213,19 @@ function renderCell(rownum, colnum) {
         cnv.style['z-index'] = '50';
         row.appendChild(cnv);
         cnv.width = 80 * w;
-        cnv.height = h + 8;
+        cnv.height = (h + 16);
     }
-    var ctx = cnv.getContext('2d');
+    ctx = cnv.getContext('2d');
     
-    var attr = conCellAttr[rownum][colnum];
-    var ch = conText[rownum].charAt(colnum);
-    var tfg = (attr & 0xFF);
-    var tbg = (attr >> 8) & 0xff;
-    var tbold  = attr & A_CELL_BOLD;
-    var stroke = h * 0.1;  // underline/strikethrough size
+    attr = conCellAttr[rownum][colnum];
+    ch = conText[rownum].charAt(colnum);
+    tfg = (attr & 0xFF);
+    tbg = (attr >> 8) & 0xff;
+    tbold  = attr & A_CELL_BOLD;
+    stroke = h * 0.1;  // underline/strikethrough size
     
     if (attr & A_CELL_REVERSE) {
-        var tmp = tfg;
+        tmp = tfg;
         tfg = tbg;
         tbg = tmp;
         if (tfg == 0)
@@ -2060,8 +2248,9 @@ function renderCell(rownum, colnum) {
             + (tbold ? 'bold ' : '') 
             + rowSize + 'px ' + fontName;
         ctx.textAlign = 'start';
-        ctx.textBaseline = 'bottom';
-    
+        ctx.textBaseline = 'top';
+        
+
         if (attr & A_CELL_GLOW) {
             // how does this work on scaled? test
             ctx.shadowColor = '#' + brightenRGB(clut[tfg], 0.25);
@@ -2084,19 +2273,19 @@ function renderCell(rownum, colnum) {
             ctx.translate(x / (size * width), 0);
             if (attr & A_CELL_OUTLINE) {
                 ctx.strokeStyle = clut[tfg];
-                ctx.lineWidth = 1;
-                ctx.strokeText(ch, 0, rowSize);
+                ctx.lineWidth = 1;q
+                ctx.strokeText(ch, 0, y);
             } else {
-                ctx.fillText(ch, 0, rowSize);
+                ctx.fillText(ch, 0, y);
             }
             ctx.restore();
         } else {
             if (attr & A_CELL_OUTLINE) {
                 ctx.strokeStyle = clut[tfg];
                 ctx.lineWidth = 1;
-                ctx.strokeText(ch, x, h);
+                ctx.strokeText(ch, x, y);
             } else {
-                ctx.fillText(ch, x, h);
+                ctx.fillText(ch, x, y);
             }
         }
     
@@ -2422,3 +2611,16 @@ function newCrsr() {
     c = getCrsrAttrColor(crsrAttr);
     o.style['background-color'] = clut[c];
 }
+
+/*
+alert(
+    'OS: ' + jscd.os +' '+ jscd.osVersion + '\n' +
+    'Browser: ' + jscd.browser +' '+ jscd.browserMajorVersion +
+      ' (' + jscd.browserVersion + ')\n' + 
+    'Mobile: ' + jscd.mobile + '\n' +
+    'Flash: ' + jscd.flashVersion + '\n' +
+    'Cookies: ' + jscd.cookies + '\n' +
+    'Screen Size: ' + jscd.screen + '\n\n' +
+    'Full User Agent: ' + navigator.userAgent
+);
+*/
